4f87986dfdc8bf23c77cb5fe4ceb285f91d1d225
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-06-29 22:35:03 -0300

    initial description page

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles.js';
+export function Details() {
+    return (
+        <Container>
+            <h1>ola</h1>
+            <span>que difícil</span>
+        </Container>
+    );
+}
