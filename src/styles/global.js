2e9c0acdfec6936336a1b0baebd4047889105249
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-07-02 22:06:18 -0300

    add font

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+    :root {
+        font-size: 62.5%;
+    }
+    
     * {
         margin: 0;
         padding: 0;
@@ -10,7 +14,15 @@ export default createGlobalStyle`
     body {
         background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
         color: ${({ theme }) => theme.COLORS.WHITE};
+
+        -webkit-font-smoothing: antialiased;
     }
+
+    body, input, button, textarea {
+        font-family: 'Roboto Slab', serif;
+        font-size: 1.6rem;
+        outline: none;
+    } 
 
     a {
         text-decoration: none;
