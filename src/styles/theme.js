e03c235e757d7ad478bca4cb02f55fcf9a8a9deb
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-07-02 21:48:08 -0300

    add theme and global style

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,15 +1,15 @@
 export default {
     COLORS: {
-        BACKGROUND_900: '#232129',
-        BACKGROUND_800: '#312E38',
-        BACKGROUND_700: '#3E3B47',
+        BACKGROUND_900: '#0D0C0F',
+        BACKGROUND_800: '#1C1B1E',
+        BACKGROUND_700: '#262529',
 
         WHITE: '#F4EDE8',
-        ORANGE: '#FF9000',
+        PINK: '#FF859B',
 
-        GRAY_100: '#999591',
-        GRAY_300: '#666360',
-
-        RED: '#FF002E',
+        GRAY_100: '#e5e5e5',
+        GRAY_200: '#CAC4CF',
+        GRAY_300: '#948F99',
+        GRAY_400: '#312E38',
     },
 };
